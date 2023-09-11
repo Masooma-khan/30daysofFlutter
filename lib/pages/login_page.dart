14b0f02447056e7f4e91cf8645d9aa131206e2f7
Masooma_khan <k.masooma198@gmail.com>
2023-09-11 16:09:14 +0500

    day  3

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,18 +3,10 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Material(
-    child: Center(
-      child: Text(
-        "Login Page",
-      style: TextStyle(
-        fontSize: 30,
-         color: Colors.redAccent,
-         fontWeight: FontWeight.bold,
-       ),
-       textScaleFactor: 5.0,
+    return Material(
+      child: Column(
+        children: [Image.asset("assets/images/login_images.png")],
       ),
-     ),
     );
   }
 }
